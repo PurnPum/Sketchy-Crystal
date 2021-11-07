2335e183347306e57a701031f0ffca2c8f598796
PurnPum <richy94@hotmail.es>
2021-11-07 21:09:49 +0100

    Changed Move Tutor so it only teaches Sketch

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -254,9 +254,9 @@ MT{02d:MT_VALUE}_MOVE = \1
 ENDM
 
 MT01 EQU const_value
-	add_mt FLAMETHROWER
-	add_mt THUNDERBOLT
-	add_mt ICE_BEAM
+	add_mt SKETCH
+	add_mt SKETCH
+	add_mt SKETCH
 NUM_TUTORS = __tmhm_value__ - NUM_TMS - NUM_HMS - 1
 
 NUM_TM_HM_TUTOR EQU NUM_TMS + NUM_HMS + NUM_TUTORS
