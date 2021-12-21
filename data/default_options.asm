0db8c977fc62aae7ffb1d57b02b030abcd645c23
PurnPum <richy94@hotmail.es>
2021-12-21 23:52:58 +0100

    Changed settings by default to Instant text and Battle Style Set
    I set the instant text by default just so people are aware it exists. If I set it to fast by default its most likely a lot of people would never check the settings to discover the instant text option.

--- data/default_options.asm
+++ data/default_options.asm
@@ -1,6 +1,13 @@
 DefaultOptions:
-; wOptions: med text speed
-	db TEXT_DELAY_FAST
+; wOptions:
+; bit 0-2: number of frames to delay when printing text
+;   inst 0; fast 1; mid 2
+; bit 3: ?
+; bit 4: no text delay
+; bit 5: stereo off/on
+; bit 6: battle style shift/set
+; bit 7: battle scene off/on
+	db %01000000 ;By default : 0 frames of delay (INST), stereo off, battle style set, battle scene on
 ; wSaveFileExists: no
 	db FALSE
 ; wTextboxFrame: frame 1
