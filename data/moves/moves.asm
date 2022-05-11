ef8601c80316dfe33a8530984c9ac20b9853799b
PurnPum <richy94@hotmail.es>
2022-05-11 04:38:29 +0200

    Updated Double Slap to only hit twice but for more damage and more accuracy

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -15,7 +15,7 @@ Moves:
 	table_width MOVE_LENGTH, Moves
 	move POUND,        EFFECT_NORMAL_HIT,         40, NORMAL,       100, 35,   0
 	move KARATE_CHOP,  EFFECT_ALWAYS_CRIT,        35, FIGHTING,     100, 20,   0
-	move DOUBLESLAP,   EFFECT_MULTI_HIT,          15, NORMAL,        85, 10,   0
+	move DOUBLESLAP,   EFFECT_DOUBLE_HIT,         25, NORMAL,       100, 10,   0
 	move COMET_PUNCH,  EFFECT_MULTI_HIT,          18, NORMAL,        85, 15,   0
 	move MEGA_PUNCH,   EFFECT_NORMAL_HIT,         80, NORMAL,        85, 20,   0
 	move LEAF_STORM,   EFFECT_SELF_SPATT_DOWN,	 115, GRASS,        100,  5, 100
