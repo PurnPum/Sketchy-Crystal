f547538fb6045835c2f43cc9bb47cb0d4c84a381
PurnPum <richy94@hotmail.es>
2022-05-13 21:03:31 +0200

    Updated Tackle

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -45,7 +45,7 @@ Moves:
 	move HORN_ATTACK,  EFFECT_NORMAL_HIT,         65, NORMAL,       100, 25,   0
 	move FURY_ATTACK,  EFFECT_TRIPLE_KICK,        15, NORMAL,       100, 20,   0
 	move UTURN,   	   EFFECT_UTURN,		      70, BUG,        	100, 20,   0
-	move TACKLE,       EFFECT_NORMAL_HIT,         35, NORMAL,        95, 35,   0
+	move TACKLE,       EFFECT_NORMAL_HIT,         40, NORMAL,       100, 35,   0
 	move BODY_SLAM,    EFFECT_PARALYZE_HIT,       85, NORMAL,       100, 15,  30
 	move WRAP,         EFFECT_TRAP_TARGET,        15, NORMAL,        85, 20,   0
 	move TAKE_DOWN,    EFFECT_RECOIL_HIT,         90, NORMAL,        85, 20,   0
