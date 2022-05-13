d5db69a83af5369c2ae76b53d65da8f7be12fe5a
PurnPum <richy94@hotmail.es>
2022-05-13 04:59:59 +0200

    Updated Rolling Kick

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -40,7 +40,7 @@ Moves:
 	move DOUBLE_KICK,  EFFECT_DOUBLE_HIT,         30, FIGHTING,     100, 30,   0
 	move MEGA_KICK,    EFFECT_NORMAL_HIT,        120, NORMAL,        75,  5,   0
 	move JUMP_KICK,    EFFECT_JUMP_KICK,          85, FIGHTING,     100, 25,   0
-	move ROLLING_KICK, EFFECT_FLINCH_HIT,         60, FIGHTING,      85, 15,  30
+	move ROLLING_KICK, EFFECT_FLINCH_HIT,         60, FIGHTING,     100, 15,  30
 	move LUNGE,  	   EFFECT_SELF_ATTACK_DOWN,  115, BUG,    	    100,  5, 100
 	move HEADBUTT,     EFFECT_FLINCH_HIT,         70, NORMAL,       100, 15,  30
 	move HORN_ATTACK,  EFFECT_NORMAL_HIT,         65, NORMAL,       100, 25,   0
