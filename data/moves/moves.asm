2d9dc35a0bdfa3d642be60fe03a3c21144ade287
PurnPum <richy94@hotmail.es>
2022-05-16 01:45:00 +0200

    Upated Fire Blast

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -139,7 +139,7 @@ Moves:
 	move SMOG,         EFFECT_ALWAYS_CRIT_POISON, 50, POISON,       100, 20,   0
 	move SLUDGE,       EFFECT_POISON_HIT,         65, POISON,       100, 20,  30
 	move BONE_CLUB,    EFFECT_FLINCH_HIT,         65, GROUND,       100, 20,  10
-	move FIRE_BLAST,   EFFECT_BURN_HIT,          120, FIRE,          85,  5,  10
+	move FIRE_BLAST,   EFFECT_BURN_HIT,          110, FIRE,          85,  5,  10
 	move WATERFALL,    EFFECT_SPLASH,	           0, WATER,        100,  1,   0
 	move CLAMP,        EFFECT_TRAP_TARGET,        35, WATER,         75, 10,   0
 	move SWIFT,        EFFECT_ALWAYS_HIT,         60, NORMAL,       100, 20,   0
