00e0224fa63208ee05e8d4cac2ac60491b97980c
PurnPum <richy94@hotmail.es>
2022-05-16 02:22:16 +0200

    Updated Dream Eater

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -151,7 +151,7 @@ Moves:
 	move SOFTBOILED,   EFFECT_HEAL,                0, NORMAL,       100, 10,   0
 	move HI_JUMP_KICK, EFFECT_JUMP_KICK,         100, FIGHTING,      90, 20,   0
 	move GLARE,        EFFECT_PARALYZE,            0, NORMAL,       100, 10,   0
-	move DREAM_EATER,  EFFECT_DREAM_EATER,       100, PSYCHIC_TYPE, 100, 15,   0
+	move DREAM_EATER,  EFFECT_DREAM_EATER,       160, PSYCHIC_TYPE, 100, 15,   0
 	move POISON_GAS,   EFFECT_STATS_DOWN_PSN,     40, POISON,       100, 20,   0
 	move BARRAGE,      EFFECT_MULTI_HIT,          15, NORMAL,        85, 20,   0
 	move LEECH_LIFE,   EFFECT_LEECH_HIT,          20, BUG,          100, 15,   0
