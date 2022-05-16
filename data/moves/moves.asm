6d70e818d192e0a8d2e9128da74368f4ad5a04b8
PurnPum <richy94@hotmail.es>
2022-05-16 02:28:20 +0200

    Updated Bonemerang

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -168,7 +168,7 @@ Moves:
 	move HYDRO_CANNON, EFFECT_SELF_SPATT_DOWN,   115, WATER,        100,  5, 100
 	move EXPLOSION,    EFFECT_SELFDESTRUCT,      250, NORMAL,       100,  5,   0
 	move FURY_SWIPES,  EFFECT_MULTI_HIT,          18, NORMAL,       100, 15,   0
-	move BONEMERANG,   EFFECT_DOUBLE_HIT,         50, GROUND,        90, 10,   0
+	move BONEMERANG,   EFFECT_DOUBLE_HIT,         50, GROUND,       100, 10,   0
 	move REST,         EFFECT_HEAL,                0, PSYCHIC_TYPE, 100, 10,   0
 	move ROCK_SLIDE,   EFFECT_FLINCH_HIT,         75, ROCK,          90, 10,  30
 	move HYPER_FANG,   EFFECT_FLINCH_HIT,         80, NORMAL,        90, 15,  10
