PurnPum/Sketchy-Crystal

commit 3514db2c41ab1a9403591471adb18c8c341919b7

Added Aqua Jet

engine/battle/effect_commands.asm
@@ -4227,7 +4227,7 @@ BattleCommand_StatUp:
 	ld a, [wFailedMessage]
 	and a
 	ret nz
-	jp MinimizeDropSub
+	;jp MinimizeDropSub
 
 RaiseStat:
 	ld a, b
@@ -4327,6 +4327,7 @@ RaiseStat:
 
 MinimizeDropSub:
 ; Lower the substitute if we're minimizing
+; Obsolete since we removed minimize
 
 	ld bc, wPlayerMinimized
 	ld hl, DropPlayerSub
@@ -4338,8 +4339,8 @@ MinimizeDropSub:
 .do_player
 	ld a, BATTLE_VARS_MOVE_ANIM
 	call GetBattleVar
-	cp MINIMIZE
-	ret nz
+	;cp MINIMIZE
+	ret 
 
 	ld a, $1
 	ld [bc], a

constants/move_constants.asm
@@ -112,7 +112,7 @@
 	const ICE_SHARD    ; 68
 	const RECOVER      ; 69
 	const DRILL_RUN    ; 6a
-	const MINIMIZE     ; 6b
+	const AQUA_JET     ; 6b
 	const SMOKESCREEN  ; 6c
 	const CONFUSE_RAY  ; 6d
 	const WITHDRAW     ; 6e

data/moves/animations.asm
@@ -108,7 +108,7 @@ BattleAnimations::
 	dw BattleAnim_IceShard
 	dw BattleAnim_Recover
 	dw BattleAnim_DrillRun
-	dw BattleAnim_Minimize
+	dw BattleAnim_AquaJet
 	dw BattleAnim_Smokescreen
 	dw BattleAnim_ConfuseRay
 	dw BattleAnim_Withdraw
@@ -2730,17 +2730,26 @@ BattleAnim_Substitute:
 	anim_wait 32
 	anim_ret
 
-BattleAnim_Minimize:
-	anim_sound 0, 0, SFX_SURF
-	anim_1gfx ANIM_GFX_HIT
-	anim_call BattleAnim_TargetObj_2Row
-	anim_minimize
-	anim_bgeffect ANIM_BG_WAVE_DEFORM_MON, $0, BG_EFFECT_USER, $0
-	anim_wait 48
-	anim_updateactorpic
-	anim_incbgeffect ANIM_BG_WAVE_DEFORM_MON
-	anim_wait 48
-	anim_call BattleAnim_ShowMon_0
+BattleAnim_AquaJet:
+	anim_3gfx ANIM_GFX_SPEED, ANIM_GFX_HIT, ANIM_GFX_WATER
+	anim_sound 0, 1, SFX_HYDRO_PUMP
+	anim_obj ANIM_OBJ_HYDRO_PUMP, 40, 110, $0
+	anim_obj ANIM_OBJ_HYDRO_PUMP, 56, 110, $0
+	anim_bgeffect ANIM_BG_WATER, $1c, $0, $0
+	anim_sound 0, 0, SFX_MENU
+	anim_bgeffect ANIM_BG_HIDE_MON, $0, BG_EFFECT_USER, $0
+	anim_obj ANIM_OBJ_SPEED_LINE, 24, 88, $2
+	anim_obj ANIM_OBJ_SPEED_LINE, 32, 88, $1
+	anim_obj ANIM_OBJ_SPEED_LINE, 40, 88, $0
+	anim_obj ANIM_OBJ_SPEED_LINE, 48, 88, $80
+	anim_obj ANIM_OBJ_SPEED_LINE, 56, 88, $81
+	anim_obj ANIM_OBJ_SPEED_LINE, 64, 88, $82
+	anim_wait 12
+	anim_sound 0, 1, SFX_COMET_PUNCH
+	anim_obj ANIM_OBJ_HIT_YFIX, 136, 56, $0
+	anim_wait 8
+	anim_bgeffect ANIM_BG_SHOW_MON, $0, BG_EFFECT_USER, $0
+	anim_wait 16
 	anim_ret
 
 BattleAnim_SkyAttack:

data/moves/descriptions.asm
@@ -107,7 +107,7 @@ MoveDescriptions::
 	dw IceShardDescription
 	dw RecoverDescription
 	dw DrillRunDescription
-	dw MinimizeDescription
+	dw AquaJetDescription
 	dw SmokescreenDescription
 	dw ConfuseRayDescription
 	dw WithdrawDescription
@@ -691,9 +691,9 @@ DrillRunDescription:
 	db   "A rotating crash"
 	next "likely to crit.@"
 
-MinimizeDescription:
-	db   "Heightens evasive-"
-	next "ness.@"
+AquaJetDescription:
+	db   "A flashing fast"
+	next "lunge. Goes first.@"
 
 SmokescreenDescription:
 	db   "Lowers the foe's"

data/moves/moves.asm
@@ -119,7 +119,7 @@ Moves:
 	move ICE_SHARD,    EFFECT_PRIORITY_HIT,       40, ICE,          100, 30,   0
 	move RECOVER,      EFFECT_HEAL,                0, NORMAL,       100, 20,   0
 	move DRILL_RUN,    EFFECT_NORMAL_HIT,         80, GROUND,       100, 10,   0
-	move MINIMIZE,     EFFECT_EVASION_UP,          0, NORMAL,       100, 20,   0
+	move AQUA_JET,     EFFECT_PRIORITY_HIT,       40, WATER,        100, 30,   0
 	move SMOKESCREEN,  EFFECT_ACCURACY_DOWN,       0, NORMAL,       100, 20,   0
 	move CONFUSE_RAY,  EFFECT_CONFUSE,             0, GHOST,        100, 10,   0
 	move WITHDRAW,     EFFECT_DEFENSE_UP,          0, WATER,        100, 40,   0

data/moves/names.asm
@@ -106,7 +106,7 @@ MoveNames::
 	li "ICE SHARD"
 	li "RECOVER"
 	li "DRILL RUN"
-	li "MINIMIZE"
+	li "AQUA JET"
 	li "SMOKESCREEN"
 	li "CONFUSE RAY"
 	li "WITHDRAW"

data/pokemon/evos_attacks2.asm
@@ -466,7 +466,7 @@ ClefairyEvosAttacks2:
 	db 4, ENCORE
 	db 8, STRUGGLE
 	db 13, DOUBLESLAP
-	db 19, MINIMIZE
+	db 19, STRUGGLE
 	db 26, DEFENSE_CURL
 	db 34, METRONOME
 	db 43, MOONLIGHT
@@ -1183,7 +1183,7 @@ GrimerEvosAttacks2:
 	db 5, STRUGGLE
 	db 10, DISABLE
 	db 16, SLUDGE
-	db 23, MINIMIZE
+	db 23, STRUGGLE
 	db 31, SCREECH
 	db 40, ACID_ARMOR
 	db 50, SLUDGE_BOMB
@@ -1198,7 +1198,7 @@ MukEvosAttacks2:
 	db 33, STRUGGLE
 	db 37, DISABLE
 	db 45, SLUDGE
-	db 23, MINIMIZE
+	db 23, STRUGGLE
 	db 31, SCREECH
 	db 45, ACID_ARMOR
 	db 60, SLUDGE_BOMB
@@ -1533,7 +1533,7 @@ ChanseyEvosAttacks2:
 	db 9, TAIL_WHIP
 	db 13, SOFTBOILED
 	db 17, DOUBLESLAP
-	db 23, MINIMIZE
+	db 23, STRUGGLE
 	db 29, STRUGGLE
 	db 35, EGG_BOMB
 	db 41, DEFENSE_CURL
@@ -1633,7 +1633,7 @@ StaryuEvosAttacks2:
 	db 19, RECOVER
 	db 25, SWIFT
 	db 31, BUBBLEBEAM
-	db 37, MINIMIZE
+	db 37, STRUGGLE
 	db 43, LIGHT_SCREEN
 	db 50, HYDRO_PUMP
 	db 0 ; no more level-up moves
@@ -2823,7 +2823,7 @@ QwilfishEvosAttacks2:
 	db 1, TACKLE
 	db 1, POISON_STING
 	db 10, STRUGGLE
-	db 10, MINIMIZE
+	db 10, STRUGGLE
 	db 19, WATER_GUN
 	db 28, PIN_MISSILE
 	db 37, TAKE_DOWN
@@ -3216,7 +3216,7 @@ BlisseyEvosAttacks2:
 	db 7, TAIL_WHIP
 	db 10, SOFTBOILED
 	db 13, DOUBLESLAP
-	db 18, MINIMIZE
+	db 18, STRUGGLE
 	db 23, STRUGGLE
 	db 28, EGG_BOMB
 	db 33, DEFENSE_CURL

commit df0c0f665d84338a6a6dd49a8ed9c363be071368

Fixed a long lasting bug introduced in commit 3514db2c41ab1a9403591471adb18c8c341919b7

engine/battle/effect_commands.asm
@@ -4249,7 +4249,7 @@ BattleCommand_StatUp:
 	ld a, [wFailedMessage]
 	and a
 	ret nz
-	;jp MinimizeDropSub
+	jp MinimizeDropSub
 
 RaiseStat:
 	ld a, b
@@ -4347,7 +4347,7 @@ RaiseStat:
 	ld [wFailedMessage], a
 	ret
 
-;MinimizeDropSub:
+MinimizeDropSub:
 ; Lower the substitute if we're minimizing
 ; Obsolete since we removed minimize
 
@@ -4362,7 +4362,7 @@ RaiseStat:
 ;	ld a, BATTLE_VARS_MOVE_ANIM
 ;	call GetBattleVar
 	;cp MINIMIZE
-;	ret 
+	ret 
 
 ;	ld a, $1
 ;	ld [bc], a