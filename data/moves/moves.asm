790563c8c43af368366cc072a93082420f31fa0f
PurnPum <richy94@hotmail.es>
2022-05-17 06:47:23 +0200

    Updated Outrage

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -213,7 +213,7 @@ Moves:
 	move DETECT,       EFFECT_PROTECT,             0, FIGHTING,     100, 10,   0
 	move MUD_BOMB,     EFFECT_SP_ATK_DOWN_HIT,    70, GROUND,       100, 10,  30
 	move LOCK_ON,      EFFECT_LOCK_ON,             0, STEEL,        100,  5,   0
-	move OUTRAGE,      EFFECT_RAMPAGE,            90, DRAGON,       100, 15,   0
+	move OUTRAGE,      EFFECT_RAMPAGE,           105, DRAGON,       100, 10,   0
 	move SANDSTORM,    EFFECT_SANDSTORM,           0, ROCK,         100, 10,   0
 	move GIGA_DRAIN,   EFFECT_LEECH_HIT,          60, GRASS,        100,  5,   0
 	move ENDURE,       EFFECT_ENDURE,              0, NORMAL,       100, 10,   0
