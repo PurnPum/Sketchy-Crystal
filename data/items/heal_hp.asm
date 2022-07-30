43330bef27c26c9f32e5786553a007a72e4fe66a
PurnPum <richy94@hotmail.es>
2022-07-30 19:12:16 +0200

    Fixed some issues and changes values to accomodate 8-bit numbers

--- data/items/heal_hp.asm
+++ data/items/heal_hp.asm
@@ -1,16 +1,16 @@
 HealingHPAmounts:
 	dbw FRESH_WATER,   45
 	dbw SODA_POP,      80
-	dbw LEMONADE,     200
+	dbw LEMONADE,     180
 	dbw HYPER_POTION, 120
 	dbw SUPER_POTION,  60
 	dbw POTION,        30
-	dbw ULTRA_POTION, 240
+	dbw ULTRA_POTION, 215
 	dbw FULL_RESTORE, MAX_STAT_VALUE
-	dbw MOOMOO_MILK,  160
+	dbw MOOMOO_MILK,  150
 	dbw BERRY,         10
 	dbw ENERGYPOWDER, 100
-	dbw ENERGY_ROOT,  300
+	dbw ENERGY_ROOT,  255
 	dbw RAGECANDYBAR,  20
 	dbw BERRY_JUICE,   30
 	dbw -1, 0 ; end
