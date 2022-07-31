91f23a5a7fae971b5a055e4bb02231d7451c2274
PurnPum <richy94@hotmail.es>
2022-07-31 21:18:22 +0200

    Map updates (mostly OW items), also reorganized the tilesets

--- maps/Route45.asm
+++ maps/Route45.asm
@@ -533,7 +533,7 @@ Route45_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  5, DARK_CAVE_BLACKTHORN_ENTRANCE, 1
+	warp_event  2,  5, DARK_CAVE_ROUTE46_ENTRANCE, 1
 
 	def_coord_events
 
