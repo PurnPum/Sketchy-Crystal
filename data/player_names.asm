c92aca0418a55e9d2d48b8e7a77ca71e6f5d252b
PurnPum <richy94@hotmail.es>
2022-08-10 05:46:59 +0200

    Changed player name options

--- data/player_names.asm
+++ data/player_names.asm
@@ -10,10 +10,10 @@ ChrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 MalePlayerNameArray:
+	db "I@"
+	db "GOLD@"
+	db "ETHAN@"
 	db "CHRIS@"
-	db "MAT@"
-	db "ALLAN@"
-	db "JON@"
 	db 2 ; title indent
 	db " NAME @" ; title
 
@@ -29,9 +29,9 @@ KrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 FemalePlayerNameArray:
+	db "I@"
 	db "KRIS@"
-	db "AMANDA@"
-	db "JUANA@"
-	db "JODI@"
+	db "LYRA@"
+	db "MARINA@"
 	db 2 ; title indent
 	db " NAME @" ; title
