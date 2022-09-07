889dfa1053b15f06d393ed17039ba37c325cbdd7
PurnPum <richy94@hotmail.es>
2022-09-07 04:51:59 +0200

    Kiln now gives you Stardust instead of Charcoal

--- maps/CharcoalKiln.asm
+++ maps/CharcoalKiln.asm
@@ -35,7 +35,7 @@ CharcoalKilnBoss:
 CharcoalKilnApprentice:
 	faceplayer
 	opentext
-	checkevent EVENT_GOT_CHARCOAL_IN_CHARCOAL_KILN
+	checkevent EVENT_GOT_STARDUST_IN_CHARCOAL_KILN
 	iftrue .YoureTheCoolest
 	checkevent EVENT_GOT_HM01_CUT
 	iftrue .Thanks
@@ -47,9 +47,9 @@ CharcoalKilnApprentice:
 .Thanks:
 	writetext CharcoalKilnApprenticeText2
 	promptbutton
-	verbosegiveitem CHARCOAL
+	verbosegiveitem STARDUST
 	iffalse .Done
-	setevent EVENT_GOT_CHARCOAL_IN_CHARCOAL_KILN
+	setevent EVENT_GOT_STARDUST_IN_CHARCOAL_KILN
 	closetext
 	end
 
@@ -125,12 +125,11 @@ CharcoalKilnApprenticeText2:
 	text "I'm sorry--I for-"
 	line "got to thank you."
 
-	para "This is CHARCOAL"
-	line "that I made."
+	para "This is STARDUST"
+	line "that I found."
 
-	para "Fire-type #MON"
-	line "would be happy to"
-	cont "hold that."
+	para "Shops would be"
+	line "happy to buy that."
 	done
 
 CharcoalKilnApprenticeText3:
