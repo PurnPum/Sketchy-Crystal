42923bd73cf75562bb534b7c66906f4d7826f21a
PurnPum <richy94@hotmail.es>
2022-12-18 11:38:15 +0100

    Updated Present's damage
    20% chance to have a 40 Base Power
    50% chance to have a 80 Base Power
    20% chance to have a 120 Base Power
    10% chance to heal the enemy

--- data/moves/present_power.asm
+++ data/moves/present_power.asm
@@ -1,6 +1,6 @@
 PresentPower:
 	; chance, power
-	db 40 percent,     40 ; 40% chance
-	db 70 percent + 1, 80 ; 30% chance
-	db 80 percent,    120 ; 10% chance
-	db -1                 ; 20% chance to heal instead
+	db 20 percent,     40 ; 20% chance
+	db 70 percent + 1, 80 ; 50% chance
+	db 90 percent,    120 ; 20% chance
+	db -1                 ; 10% chance to heal instead
